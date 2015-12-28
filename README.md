603e2ed762808059feb0433fa257235d426f4eea
Matt LeBrun <lebrun@adobe.com>
2015-12-28 13:19:55 -0500

    Logger: added Winston, default console log level in ENV config, listening to `leaveallrooms` event

--- README.md
+++ README.md
@@ -9,7 +9,11 @@ Getting Started
 
 ### Install
 
-`git clone` the project.
+`git clone` the project
+
+In the projects root, you can generate a test key and cert with:
+
+`sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout dev.key -out dev.crt`
 
 ```
 npm install
--- index.js
+++ index.js
@@ -1,25 +1,42 @@
 /*globals __dirname, console, process */
-var http = require('http'),
+var fs = require('fs'),
+    https = require('https'),
     express = require('express'),
     Primus = require('primus'),
     Rooms = require('primus-rooms'),
-    Notify = require('notify'),
     RedisSubscriber = require('./lib/subscribers/RedisSubscriber'),
     Application = require('./lib/Application'),
+    logger = require(__dirname + '/lib/logger'),
     config = process.env,
-    server, primus, app, logger;
+    server, primus, app;
 
-server = require('http').createServer(express());
-primus = new Primus(server, { transformer: config.CFG_PRIMUS_TRANSFORMER });
+server = https.createServer({
+  key: fs.readFileSync(config.CFG_PRIMUS_PATH_KEY),
+  cert: fs.readFileSync(config.CFG_PRIMUS_PATH_CERT),
+  requestCert: true,
+  rejectUnauthorized: false
+}, express());
 
-subscriber = new RedisSubscriber({ redis: { port: config.CFG_REDIS_PORT, host: config.CFG_REDIS_HOST } });
+primus = new Primus(server, {
+  transformer: config.CFG_PRIMUS_TRANSFORMER,
+  origins: 'https://net.dev.be.lan'
+});
+
+subscriber = new RedisSubscriber({
+  port: config.CFG_REDIS_PORT,
+  host: config.CFG_REDIS_HOST
+});
 application = new Application(primus, subscriber);
 
 // save latest client library
 primus.save(__dirname +'/client.js');
 
-subscriber.connect();
+process.on('SIGTERM', function () {
+  server.close(function () {
+    process.exit(0);
+  });
+});
 
 server.listen(config.CFG_PRIMUS_PORT, function() {
-  console.log('Listening on: ' + config.CFG_PRIMUS_PORT);
+  logger.info('[ Primus Listening ] ' + config.CFG_PRIMUS_PORT);
 });
